1349277a5772ae93abd329e3f7d9095d35b0ff19
Florian <florian.berghuber@gmail.com>
2020-12-03 13:27:43 +0100

    added Start Screen

--- hsd-bingo/src/BingoApp/pages/Start.js
+++ hsd-bingo/src/BingoApp/pages/Start.js
@@ -1,12 +1,80 @@
 import React from 'react';
-import { Button } from 'semantic-ui-react';
+import { Button, Dropdown, Header, Input, Grid } from 'semantic-ui-react';
+
+const SemesterOptions = [
+    {key: 'HSD1', text: 'HSD Semester 1'},
+    {key: 'HSD2', text: 'HSD Semester 2'},
+    {key: 'HSD3', text: 'HSD Semester 3'},
+    {key: 'HSD4', text: 'HSD Semester 4'},
+    {key: 'HSD5', text: 'HSD Semester 5'},
+    {key: 'ESD1', text: 'ESD Semester 1'},
+    {key: 'ESD2', text: 'ESD Semester 2'},
+    {key: 'ESD3', text: 'ESD Semester 3'},
+]
+
+var SubjectOptions = [
+    {text: 'DIG1'},
+    {text: 'SEN1'},
+    {text: 'CEC1'},
+]
+
+class Headline extends React.Component {
+    render(){
+        return (
+            <Header as='h1'>HSD - BINGO</Header>
+        );
+    }
+}
+
+class Namefield extends React.Component {
+    render(){
+        return (
+            <Input focus placeholder='Benutzername' />
+        );
+    }
+}
+
+class Semester extends React.Component {
+    render(){
+        return (
+            <Dropdown
+            placeholder='Semester Wählen'
+            selection
+            options={SemesterOptions}
+            />
+        );
+    }
+}
+
+class Subjects extends React.Component {
+    render(){
+        return(
+            <Dropdown
+            placeholder='Fach Wählen'
+            selection
+            options={SubjectOptions}
+            />
+        );
+    }
+}
+
+class Submit extends React.Component {
+    render(){
+        return(
+            <Button primary>Start</Button>
+        )
+    }
+}
 
 class Start extends React.Component {
     render(){
-        return (
-            <div>
-                <Button>Start</Button>
-            </div>
+        return (<Grid centered>
+                    <Headline />
+                    <Grid.Row> <Namefield /> </Grid.Row>
+                    <Grid.Row> <Semester /> </Grid.Row>
+                    <Grid.Row> <Subjects />  </Grid.Row>
+                    <Grid.Row> <Submit /> </Grid.Row>
+                </Grid>
           );
     }
 }
