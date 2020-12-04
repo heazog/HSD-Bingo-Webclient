00b4780f5a5167ee11d4c4890cca3b077cc4d097
Markus <S2019002131@students.fh-hagenberg.at>
2020-12-04 22:49:23 +0100

    [LOBBY] upload lobby.js again...

--- hsd-bingo/src/BingoApp/pages/Lobby.js
+++ hsd-bingo/src/BingoApp/pages/Lobby.js
@@ -1,130 +1,232 @@
 import React, { Component } from 'react';
-import { Header, Button, Table, Input, Grid } from 'semantic-ui-react'
+import { Container, Header, Button, Table, Icon, Grid } from 'semantic-ui-react'
+import MyData from '../Data'
 
-class ButtonToggle extends Component {
-  state = {}
-  handleClick = () =>
-    this.setState((prevState) => ({ active: !prevState.active }))
+const checkInterval = 500;
 
-
-  render() {
-    const { active } = this.state
-
-    return (
-      <Button toggle active={active} onClick={this.handleClick}>
-        Bereit
-      </Button>
-    )
-  }
+function sleep() {
+    return new Promise(resolve => setTimeout(resolve, checkInterval));
 }
 
-const TablePlayerLobby = () => (
-  <Table unstackable celled>
-    <Table.Header>
-      <Table.Row>
-        <Table.HeaderCell>Spieler</Table.HeaderCell>
-        <Table.HeaderCell>Bereit</Table.HeaderCell>
-      </Table.Row>
-    </Table.Header>
+class Lobby extends Component {
+  constructor(props) {
+    super(props);
 
-    <Table.Body>
-      <Table.Row>
-        <Table.Cell>Drack</Table.Cell>
-        <Table.Cell>X</Table.Cell>
-      </Table.Row>
-      <Table.Row>
-        <Table.Cell>Flo</Table.Cell>
-        <Table.Cell>-</Table.Cell>
-      </Table.Row>
-      <Table.Row>
-        <Table.Cell>Mario</Table.Cell>
-        <Table.Cell>X</Table.Cell>
-      </Table.Row>
-    </Table.Body>
+    this.state = {
+      lobby: "",
+      name: "",
+      id: "",
+      master: false,
+      ready: false,
+      players: [],
+      error: false,
+      started: false
+    };
 
-    <Table.Footer fullWidth>
-      <Table.Row>
-        <Table.HeaderCell>
-          <ButtonToggle>Bereit</ButtonToggle>
-        </Table.HeaderCell>
-        <Table.HeaderCell />
-      </Table.Row>
-    </Table.Footer>
-  </Table>
-)
+    this.loadPlayers = this.loadPlayers.bind(this);
+    this.toggleReady = this.toggleReady.bind(this);
+    this.leaveLobby = this.leaveLobby.bind(this);
+    this.printReady = this.printReady.bind(this);
+    this.printOwnName = this.printOwnName.bind(this);
 
-const TableWordsLobby = () => (
-  <Table compact celled>
-    <Table.Header>
-      <Table.Row>
-        <Table.HeaderCell>Wörterliste</Table.HeaderCell>
-      </Table.Row>
-    </Table.Header>
+    this.loadPlayers();
+    this.loadUser();
+    this.loadLobby();
+  }
 
-    <Table.Body>
-      <Table.Row>
-        <Table.Cell>Soooo</Table.Cell>
-      </Table.Row>
-      <Table.Row>
-        <Table.Cell>Dirac Impuls</Table.Cell>
-      </Table.Row>
-    </Table.Body>
-
-    <Table.Footer fullWidth>
-      <Table.Row>
-        <Table.HeaderCell>
-          <Input placeholder='Wort eingeben...' />
-        </Table.HeaderCell>
-      </Table.Row>
-    </Table.Footer>
-  </Table>
-)
-
-const GridTables = () => (
-  <Grid divided='vertically'>
-    <Grid.Row columns={2} only='computer'>
-      <Grid.Column>
-        {TablePlayerLobby()}
-      </Grid.Column>
-      <Grid.Column>
-       {TableWordsLobby()}
-      </Grid.Column>
-    </Grid.Row>
-    <Grid.Row columns={1} only='tablet mobile'>
-      <Grid.Column>
-       {TablePlayerLobby()}
-      </Grid.Column>
-    </Grid.Row>
-    <Grid.Row columns={1} only='tablet mobile'>
-      <Grid.Column>
-       {TableWordsLobby()}
-      </Grid.Column>
-    </Grid.Row>
-  </Grid>
-)
-
-const GridHeader = () => (
-  <Grid divided='vertically'>
-    <Grid.Row columns={2}>
-      <Grid.Column>
-        <Header as='h1'>Lobby ISE</Header>
-      </Grid.Column>
-      <Grid.Column>
-        <Button color='red' floated='right'>Leave Lobby</Button>
-      </Grid.Column>
-    </Grid.Row>
-  </Grid>
-)
-
-class Lobby extends React.Component {
-    render(){
-        return (
-            <div>
-                {GridHeader() /* H1 innerhalb von <p></p> is verboten :D */}
-                {GridTables()}
-            </div>
-          );
+  async loadPlayers(){
+    let players_list = await MyData.getPlayers();
+    
+    if(players_list === null){
+        this.setState({error: true});
+    }else{
+        this.setState({players: players_list, error: false});
     }
+    await sleep();
+  }
+
+  async loadUser(){
+    let user = await MyData.getUser();
+
+    if(user === null){
+        this.setState({error: true});
+    }else{
+        this.setState({name: user.name, master: user.master, error: false});
+    }
+    await sleep();
+  }
+
+  async loadLobby(){
+    let lobby = await MyData.getLobby();
+
+    if(lobby === null){
+        this.setState({error: true});
+    }else{
+        this.setState({lobby: lobby, error: false});
+    }
+    await sleep();
+  }
+  
+
+  toggleReady() {
+    this.setState((prevState) => ({ ready: !prevState.ready }))
+    if(this.state.master === true)
+    {
+      this.setState({started: true});
+      //start game
+    }
+  }
+
+  leaveLobby() {
+    
+  }
+
+
+  GridMaster = () => (
+    <Grid divided='vertically'>
+      <Grid.Row columns={2}>
+        <Grid.Column width={1}>
+          <Icon color="yellow" name = "chess king" />
+        </Grid.Column>
+        <Grid.Column>
+          {this.state.name}
+        </Grid.Column>
+      </Grid.Row>
+    </Grid>
+  )
+  printOwnName(){
+    if(this.state.master === true) {
+          return(
+            this.GridMaster()
+          )
+    }else {
+          return(
+            this.state.name
+          )
+     }
+  }
+
+  printReady(state){
+    if(state === true) {
+          return(
+            <Icon color="green" name = "thumbs up outline" />
+          )
+    }else {
+          return(
+            <Icon color="red" name = "thumbs down outline" />
+          )
+     }
+  }
+
+  TablePlayerLobby = () => (
+    <Table unstackable celled>
+      <Table.Header>
+        <Table.Row>
+          <Table.HeaderCell>Spieler</Table.HeaderCell>
+          <Table.HeaderCell>Bereit</Table.HeaderCell>
+        </Table.Row>
+      </Table.Header>
+
+      <Table.Body>
+        <Table.Row>
+          <Table.Cell>{this.printOwnName()}</Table.Cell>
+          <Table.Cell>{this.printReady(this.state.ready)}</Table.Cell>
+        </Table.Row>
+
+        {this.state.players.map((player) => {
+          var status = player.status === "ready";
+            return (
+            <Table.Row>
+              <Table.Cell>{player.name}</Table.Cell>
+              <Table.Cell>{this.printReady(status)}</Table.Cell>
+            </Table.Row>
+          )
+          })}
+      </Table.Body>
+
+      <Table.Footer fullWidth>
+        <Table.Row>
+          <Table.HeaderCell>
+            <Button toggle active={this.state.ready} onClick={this.toggleReady}>Bereit</Button>
+          </Table.HeaderCell>
+          <Table.HeaderCell />
+        </Table.Row>
+      </Table.Footer>
+    </Table>
+  )
+
+  GridHeader = () => (
+    <Grid divided='vertically'>
+      <Grid.Row columns={2}>
+        <Grid.Column>
+          <Header as='h1'>{this.state.lobby}</Header>
+        </Grid.Column>
+        <Grid.Column>
+          <Button color='red' floated='right' circular onClick={this.leaveLobby}>Leave Lobby</Button>
+        </Grid.Column>
+      </Grid.Row>
+    </Grid>
+  )
+
+  /*
+  const TableWordsLobby = () => (
+    <Table compact celled>
+      <Table.Header>
+        <Table.Row>
+          <Table.HeaderCell>Wörterliste</Table.HeaderCell>
+        </Table.Row>
+      </Table.Header>
+
+      <Table.Body>
+        <Table.Row>
+          <Table.Cell>Soooo</Table.Cell>
+        </Table.Row>
+        <Table.Row>
+          <Table.Cell>Dirac Impuls</Table.Cell>
+        </Table.Row>
+      </Table.Body>
+
+      <Table.Footer fullWidth>
+        <Table.Row>
+          <Table.HeaderCell>
+            <Input placeholder='Wort eingeben...' />
+          </Table.HeaderCell>
+        </Table.Row>
+      </Table.Footer>
+    </Table>
+  )
+
+  const GridTables = () => (
+    <Grid divided='vertically'>
+      <Grid.Row columns={2} only='computer'>
+        <Grid.Column>
+          {TablePlayerLobby()}
+        </Grid.Column>
+        <Grid.Column>
+        {TableWordsLobby()}
+        </Grid.Column>
+      </Grid.Row>
+      <Grid.Row columns={1} only='tablet mobile'>
+        <Grid.Column>
+        {TablePlayerLobby()}
+        </Grid.Column>
+      </Grid.Row>
+      <Grid.Row columns={1} only='tablet mobile'>
+        <Grid.Column>
+        {TableWordsLobby()}
+        </Grid.Column>
+      </Grid.Row>
+    </Grid>
+  )*/
+
+  render(){
+      return (
+          <Container className="Lobby">
+              {this.GridHeader()}
+              {this.TablePlayerLobby()}
+          </Container>
+        );
+  }
 }
 
 export default Lobby;
