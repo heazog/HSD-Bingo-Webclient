4130a4ce222e53329b42f722e980b0b88c8686fc
Daniel Herzog <herzog@io.at>
2020-12-14 17:09:10 +0100

    [Data] added comments, small changes 2
    - added convertLVA method
    - changed bingo parameter to UID
    - added disconnect

--- hsd-bingo/src/BingoApp/Data.js
+++ hsd-bingo/src/BingoApp/Data.js
@@ -1,11 +1,16 @@
 import axios from 'axios';
 
+// debug == false: Data class will be used (with server connection)
+// debug == true: DummyData class will be used (without server connection)
 const debug = false;
+
+// Sever address of REST-API service
 const SERVER = "http://193.170.192.202:8080";
 
 class DataBase{
 
     constructor(){
+        // initializing member variables
         this.board = null;
         this.lobby = null;
         this.userName = null;
@@ -14,66 +19,43 @@ class DataBase{
         this.master = null;
     }
 
-
+    // wrapper for server communication
     async getLobbies(){ throw new Error('getLobbies not implemented!'); }
     async joinLobby(lobby, username){ throw new Error('joinLobby not implemented!'); }
     async getPlayers(){ throw new Error('getPlayers not implemented!'); }
     async getJoinedLobbyStatus(){ throw new Error('getLobbyStatus not implemented!'); }
-    async getLobbyStatus(){ throw new Error('getLobbyStatus not implemented!'); }
+    async getLobbyStatus(lobby){ throw new Error('getLobbyStatus not implemented!'); }
     async start(){ throw new Error('start not implemented!'); }
     async requestBoard(){ throw new Error('requestBoard not implemented!'); }
     async makeSelection(x, y){ throw new Error('makeSelection not implemented!'); }
     async bingo(){ throw new Error('bingo not implemented!'); }
     async highscores(){ throw new Error('highscores not implemented!'); }
     async disconnect(){ throw new Error('disconnect not implemented!'); }
+
+    // get functions for member Variables
     getWinner(){ return this.winnerName; }
     getUser(){ return this.userName; }
     getMaster(){ return this.master; }
     getLobby(){ return this.lobby; }
-    getBoard(){ 
-        return this.board;
-        /*return [
-            [
-                "Ähm",
-                "Also",
-                "Any questions?",
-                "Äquivalentes Basisband",
-                "AWGN"
-            ],
-            [
-                "Bandbreite",
-                "Bandbreiteneffizienz",
-                "Basisband",
-                "Betrag+Phase",
-                "Bodediagramm"
-            ],
-            [
-                "Mach ma mal weiter",
-                "Matched Filter",
-                "Mättlab",
-                "Mobilfunk",
-                "Multiplikativer Faktor"
-            ],
-            [
-                "Nachrichtentechnik",
-                "Nebenfrequenzen",
-                "Normalisiert",
-                "Nutzsignal",
-                "Nyquist/ -kriterium / -theorem"
-            ],
-            [
-                "Schuldigung",
-                "Sendeimpuls",
-                "So/Soo",
-                "Spektrum",
-                "Symbolperiode"
-            ]
-        ];*/
+    getBoard(){ return this.board; }
+
+    // Convert LVA name from alias to Display name (e.g. from ISE7 to ISE1)
+    convertLVA(lva){
+        let semesterNo = lva.slice(-1);
+        if(semesterNo >= 7){
+            return lva.slice(0,-1) + (semesterNo - 6);
+        }else if(semesterNo === 0){ // Semester 10
+            return lva.slice(0,-2) + 4;
+        }else{
+            return lva.slice(0,-1) + semesterNo;
+        }
     }
 };
 
 
+
 class Data extends DataBase{
+    // help function for HTTP GET requests
     async get(path){
         var data = [];
         await axios.get(SERVER + path)
@@ -85,6 +67,7 @@ class Data extends DataBase{
           });
         return data;
     }
+    // help function for HTTP POST requests
     async post(path, postData){
         var data = [];
         await axios.post(SERVER + path, postData)
@@ -96,9 +79,11 @@ class Data extends DataBase{
           });
         return data;
     }
+    // returns /getlobbies server response
     async getLobbies(){  
         return await this.get("/getlobbies");
     }
+    // returns /joinlobby server response and sets member variables
     async joinLobby(lobby, username){
         var ret = await this.post("/joinlobby", {"lobby": lobby, "name": username});
         this.lobby = lobby;
@@ -107,25 +92,30 @@ class Data extends DataBase{
         this.master = ret.master;
         return ret;
     }
+    // returns /getplayers server response if lobby is already set, else null
     async getPlayers(){ 
         if(!this.lobby)
             return null;
         return await this.get("/getplayers?lobby="+this.lobby);
     }
+    // returns /getlobbystatus server response if lobby is already set, else null
     async getJoinedLobbyStatus(){
         if(!this.lobby)
             return null;
         return await this.get("/getlobbystatus?lobby="+this.lobby);
     }
+    // returns /getlobbystatus server response of parameter 'lobby'
     async getLobbyStatus(lobby){
         return await this.get("/getlobbystatus?lobby="+lobby);
     }
+    // returns /start server response if userID is already set, else null
     async start(){
         if(!this.userID)
             return null;
         await this.post("/start", {"UID": this.userID});
         return true;
     }
+    // returns /getboard server response if userID is already set, else null
     async requestBoard(){ 
         if(!this.userID)
             return null;
@@ -143,36 +133,44 @@ class Data extends DataBase{
         console.log(this.board);
         return true;
     }
+    // returns /makeselection server response if userID is already set, else null
     async makeSelection(x, y){ 
         if(!this.userID)
             return null;
         await this.post("/makeselection", {"UID": this.userID, "x": x, "y": y});
         return true;
     }
+    // returns /bingo server response if userID is already set, else null
     async bingo(){ 
-        if(!this.lobby)
+        if(!this.userID)
             return null;
-        var ret = await this.get("/bingo?lobby="+this.lobby);
+        var ret = await this.get("/bingo?UID="+this.userID);
         this.winnerName = ret.winner;
         return ret;
     }
+    // returns /highscores server response
     async highscores(){ 
         return await this.get("/highscores");
     }
+    // returns /disconnect server response if lobby is already set, else null
     async disconnect(){
         if(!this.lobby)
             return null;
         console.log({"lobby": this.lobby, "uid": this.userID});
-        var ret = await this.post("/disconnect", {"lobby": this.lobby, "UID": this.userID});
-        return ret;
+        return await this.post("/disconnect", {"lobby": this.lobby, "UID": this.userID});
     }
 }
 
+
+//function for simulating request time
 function sleep(ms) {
     return new Promise(resolve => setTimeout(resolve, ms));
 }
 
+// Class for server simulation
 class DataDummy extends DataBase{
+
+    // returns a simulated /getlobbies response (random LVAs and users)
     async getLobbies(){
 
         let DummyLobbie = [];
@@ -191,20 +189,22 @@ class DataDummy extends DataBase{
         return DummyLobbie;
     }
 
+    // returns a simulated /joinlobby response and sets member variables
     async joinLobby(lobby, username){
         await sleep(1000);
         this.lobby = lobby;
         this.userName = username;
         this.userID = 3;
         this.master = true;
-        return [
-            {
+        return {
                 "UID": 3,
                 "master": true
-            }
-        ];
+            };
     }
+    // returns a simulated /getplayers response if lobby is already set, else null
     async getPlayers(){
+        if(!this.lobby)
+            return null;
         await sleep(1000);
         return [
             {
@@ -219,24 +219,34 @@ class DataDummy extends DataBase{
             }
         ];
     }
+    // returns a simulated /getlobbystatus response if lobby is already set, else null
     async getJoinedLobbyStatus(){
+        if(!this.lobby)
+            return null;
         await sleep(1000);
         return {
             "gametime": "34"
         };
     }
-    async getLobbyStatus(){
+    // returns a simulated /getlobbystatus response of parameter 'lobby'
+    async getLobbyStatus(lobby){
         await sleep(1000);
         return {
             "gametime": "99"
         };
     }
+    // returns a simulated /start response if userID is already set, else null
     async start(){
+        if(!this.userID)
+            return null;
         await sleep(1000);
         return true;
     }
-
+    
+    // returns a simulated /getboard response if userID is already set, else null
     async requestBoard(){
+        if(!this.userID)
+            return null;
         await sleep(1000);
         this.board = [
             [
@@ -277,16 +287,23 @@ class DataDummy extends DataBase{
         ];
         return true;
     }
+    // returns a simulated /makeselection response if userID is already set, else null
     async makeSelection(x, y){
+        if(!this.userID)
+            return null;
         await sleep(1000);
         return true;
     }
+    // returns a simulated /bingo response if userID is already set, else null
     async bingo(){
+        if(!this.userID)
+            return null;
         await sleep(1000);
         return {
             "winner": null
         }
     }
+    // returns a simulated /highscores response
     async highscores(){
         await sleep(1000);
         return { 
@@ -300,7 +317,10 @@ class DataDummy extends DataBase{
             ]
         }
     }
+    // returns a simulated /disconnect server response if lobby is already set, else null
     async disconnect(){
+        if(!this.lobby)
+            return null;
         await sleep(1000);
         return true;
     }
@@ -309,9 +329,11 @@ class DataDummy extends DataBase{
 
 var MyData;
 if(debug){
-  MyData = new DataDummy();
+    // use dummy Class if debug
+    MyData = new DataDummy();
 }else{
-  MyData = new Data();
+    // use Data Class if not debug
+    MyData = new Data();
 }
 
 
