81197fa34bb4352f811aea5aa689a99fede97a9e
Daniel Herzog <herzog@io.at>
2020-12-14 19:53:36 +0100

    [DATA] added Tests

--- hsd-bingo/src/BingoApp/BingoApp.test.js
+++ hsd-bingo/src/BingoApp/BingoApp.test.js
@@ -1,8 +1,7 @@
 import { render, screen } from '@testing-library/react';
 import BingoApp from './BingoApp';
 
-test('renders learn react link', () => {
-  render(<BingoApp />);
-  const linkElement = screen.getByText(/Hallo/i);
-  expect(linkElement).toBeInTheDocument();
+test('renders Bingo App', () => {
+  const { container } = render(<BingoApp />);
+  expect(container.firstChild.classList.contains('BingoApp')).toBe(true)
 });
